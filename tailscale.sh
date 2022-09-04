a2efa20f5bcbb60ada562286c79ee4fec3784b95
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-09-04 03:22:55 +0200

    enabled port forwording

--- tailscale.sh
+++ tailscale.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "================================================================================"
-echo "Adding Tailscale’s package signing key and repository"
+echo "Adding Tailscales package signing key and repository"
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/jammy.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
@@ -21,3 +21,10 @@ sudo tailscale status
 echo "================================================================================"
 echo "Tailscale IPv4"
 sudo tailscale ip -4
+
+echo "================================================================================"
+echo "Enabling IP forwarding"
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+sudo tailscale up --advertise-routes=192.168.0.0/24 --accept-dns=true
